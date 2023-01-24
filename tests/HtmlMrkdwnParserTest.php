c1f382a6494973dcb4d152fb840c9b6a07e116c6
Alexander Nitsche <neos@alexandernitsche.com>
2023-01-24 09:52:48 +0100

    TASK: Use generic question data in HtmlMrkdwnParserTest.php

--- tests/HtmlMrkdwnParserTest.php
+++ tests/HtmlMrkdwnParserTest.php
@@ -12,36 +12,29 @@ final class HtmlMrkdwnParserTest extends AbstractTestCase
      */
     public function testParse(): void
     {
-        $stackOverflowQuestionBody = <<<'NOWDOC'
-<p>Is the document tree cached in the Neos CMS backend? I have created two pages under &quot;Home&quot;: &quot;Neos CMS&quot; and &quot;Blog&quot; and they are displayed correctly in the main menu of the page preview, but when I click on one of the two page nodes in the document tree, the &quot;Blog&quot; node disappears in the document tree. Only when I click on the &quot;Home&quot; node or the tree refresh button, the &quot;Blog&quot; node temporarily reappears. When flushing the cache via CLI command <code>./flow flow:cache:flush</code> the node becomes permanently visible.</p>
-<p>This behavior is browser independent; normally I use Firefox, just now I used Vivaldi with default settings.</p>
-<p>I used the <a href="https://github.com/code-q-web-factory/Neos-Skeleton" rel="nofollow noreferrer">CodeQ Skeleton</a> as the base distribution, but others have confirmed the behavior for the official <a href="https://github.com/neos/neos-base-distribution" rel="nofollow noreferrer">Neos Base Distribution</a> as well.</p>
-<p>I can provide a bash script that builds up a Neos instance for repeating reproduction of this behaviour.</p>
-<p><strong>Bold</strong> <em>italic</em> <code>inline code</code></p>
+        $questionBody = <<<'NOWDOC'
+<p>Block text with <a href="https://www.neos.io/" title="Neos CMS - Open Source Content Application Framework" rel="nofollow noreferrer">link</a>.</p>
+<p>Block text with &quot;quotation&quot;.</p>
+<p>Block text with <code>$inline = &quot;code&quot;;</code>.</p>
+<p>Block text with <strong>bold</strong> and <em>italic</em>.</p>
 <pre class="lang-php prettyprint-override"><code>$block = &quot;code&quot;;
 </code></pre>
 <ol>
-<li>List item 1</li>
-<li>List item 2</li>
+<li>Numbered list item 1</li>
+<li>Numbered list item 2</li>
 </ol>
 <ul>
 <li>List item 3</li>
 <li>List item 4</li>
 </ul>
-<ol>
-<li>List item 5</li>
-<li>List item 6</li>
-</ol>
-<ul>
-<li>List item 7</li>
-<li>List item 8</li>
-</ul>
-<h2>Heading</h2>
+<h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
 <hr />
 <p><div class="snippet" data-lang="js" data-hide="false" data-console="true" data-babel="false">
 <div class="snippet-code">
 <pre class="snippet-code-html lang-html prettyprint-override"><code>&lt;html&gt;
-&lt;body&gt;Test&lt;/body&gt;
+&lt;body&gt;Code snippet&lt;/body&gt;
 &lt;/html&gt;</code></pre>
 </div>
 </div>
@@ -49,44 +42,40 @@ final class HtmlMrkdwnParserTest extends AbstractTestCase
 NOWDOC;
 
         $expected = <<<'NOWDOC'
-Is the document tree cached in the Neos CMS backend? I have created two pages under "Home": "Neos CMS" and "Blog" and they are displayed correctly in the main menu of the page preview, but when I click on one of the two page nodes in the document tree, the "Blog" node disappears in the document tree. Only when I click on the "Home" node or the tree refresh button, the "Blog" node temporarily reappears. When flushing the cache via CLI command `./flow flow:cache:flush` the node becomes permanently visible.
+Block text with <https://www.neos.io/|link>.
 
-This behavior is browser independent; normally I use Firefox, just now I used Vivaldi with default settings.
+Block text with "quotation".
 
-I used the <https://github.com/code-q-web-factory/Neos-Skeleton|CodeQ Skeleton> as the base distribution, but others have confirmed the behavior for the official <https://github.com/neos/neos-base-distribution|Neos Base Distribution> as well.
+Block text with `$inline = "code";`.
 
-I can provide a bash script that builds up a Neos instance for repeating reproduction of this behaviour.
-
-*Bold* _italic_ `inline code`
+Block text with *bold* and _italic_.
 
 ```
 $block = "code";
 ```
 
-1. List item 1
-2. List item 2
+1. Numbered list item 1
+2. Numbered list item 2
 
 - List item 3
 - List item 4
 
-1. List item 5
-2. List item 6
+*Heading 1*
 
-- List item 7
-- List item 8
+*Heading 2*
 
-*Heading*
+*Heading 3*
 
 ---
 
 ```
 &lt;html&gt;
-&lt;body&gt;Test&lt;/body&gt;
+&lt;body&gt;Code snippet&lt;/body&gt;
 &lt;/html&gt;
 ```
 NOWDOC;
 
-        $actual = (new HtmlMrkdwnParser())->parse($stackOverflowQuestionBody);
+        $actual = (new HtmlMrkdwnParser())->parse($questionBody);
         $this->assertEquals($expected, $actual);
     }
 }
